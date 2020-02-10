95cc8b40e023711f09538668f8bea4f2dbfe382e
Jacek Pietras <jaxilus@gmail.com>
2020-02-10 11:44:45 +0100

    Creating a Quiz component

---
+++ src/Quiz.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+    render() {
+        return <div>
+                    <div class="QuizQuestion">
+                        {quizData.quiz_questions[0].instruction_text}
+                    </div>
+                </div>
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = { quiz_position: 1 };
+    }
+}
+
+export default Quiz;
